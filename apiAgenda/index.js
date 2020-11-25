0dfaf84c19180aeb4365202a08a7f597a0d7a9e8
Matheus-Borges-EM <matheus.borges@escolarmanager.com.br>
2020-11-24 21:00:45 -0300

    Instala dependências e projeto de api

--- apiAgenda/index.js
+++ apiAgenda/index.js
@@ -1,8 +1,23 @@
-import express from 'express';
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = 3333;
 const app = express();
 
-app.get("/", (req, res) => res.json("ntask api"));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(PORT, () => console.log(`NTask API - Porta ${PORT}`));
+app.get("/index", (req, res) => {
+    res.json({
+        tasks: [
+            {title: "teste 1"},
+            {title: "teste 2"},
+        ]
+    });
+});
+
+app.listen(PORT, () => console.log(`Api rodando na porta ${PORT} :3`));
