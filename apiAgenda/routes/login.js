986980f2a2feccc07b26fdfe79ee160b256b6384
Matheus-Borges-EM <matheus.borges@escolarmanager.com.br>
2021-01-19 21:06:56 -0300

    Adiciona método post na rota de login para autenticação

--- apiAgenda/routes/login.js
+++ apiAgenda/routes/login.js
@@ -9,4 +9,33 @@ module.exports = app => {
                 res.status(412).json({ msg: error.message });
             });
     });
+
+    app.post("/Login", (req, res) => {
+
+        const usuario = req.body.login;
+        const senha = req.body.senha;
+
+        if (usuario && senha) {
+            autentiqueUsuario(Users, usuario, senha, res);
+        }
+        else {
+            res.sendStatus(404);
+        }
+    });
+}
+
+function autentiqueUsuario(Users, usuario, senha, res) {
+    
+    Users.findOne({ where: { login: usuario } })
+        .then(user => {
+            if (user.senha == senha) {
+                res.sendStatus(200);
+            }
+            else {
+                res.sendStatus(401);
+            }
+        })
+        .catch(error => {
+            res.status(401).json({ msg: error.message });
+        });
 }
