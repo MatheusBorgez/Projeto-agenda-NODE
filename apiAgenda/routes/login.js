62ffb9b112fd46f482d601b180d24ede91af04c1
Matheus-Borges-EM <matheus.borges@escolarmanager.com.br>
2021-01-20 21:20:34 -0300

    Remove função desnecessária

--- apiAgenda/routes/login.js
+++ apiAgenda/routes/login.js
@@ -2,14 +2,6 @@ module.exports = app => {
 
     const Users = app.db.models.User;
 
-    app.get("/Login/:id", (req, res) => {
-        Users.findOne({ where: req.params })
-            .then(result => res.json(result))
-            .catch(error => {
-                res.status(412).json({ msg: error.message });
-            });
-    });
-
     app.post("/Login", (req, res) => {
 
         const usuario = req.body.login;
