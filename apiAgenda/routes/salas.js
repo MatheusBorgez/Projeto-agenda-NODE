87bdc290f4620723986c05e35f20326b0558a2f9
Matheus-Borges-EM <matheus.borges@escolarmanager.com.br>
2021-02-04 22:16:10 -0300

    Cria get e post para tela de inserção dos horários

--- apiAgenda/routes/salas.js
+++ apiAgenda/routes/salas.js
@@ -1,10 +1,68 @@
 module.exports = app => {
-    const Users = app.db.models.User;
     const Alunos = app.db.models.Aluno;
+    const Horarios = app.db.models.Horario;
 
-    app.route("/administracao/:id")
+    app.route("/sala/:id/:sala")
         .get((req, res) => {
-            obtenhaAluno(Alunos, req, res);
+            obtenhaHorariosAluno(Horarios, req, res);
+        })
+        .post((req, res) => {
+            insiraOuAtualize(Horarios, req, res);
+        })
+
+
+
+}
+
+function insiraOuAtualize(Horarios, req, res) {
+    Horarios.findOne({
+        where: {
+            idAluno: req.body.id,
+            sala: req.body.sala
+        }
+    })
+        .then(
+            function (result) {
+                if (result) {
+                    atualizeHorarios(Horarios, req, res);
+                }
+                else {
+                    insiraHorarios(Horarios, req, res);
+                }
+            }
+        )
+        .catch(error => envieMensagemErro(res, error));
+}
+
+function atualizeHorarios(Horarios, req, res) {
+    req.body.horarios.forEach(horario => {
+        Horarios.update(horario, {
+            where: {
+                idAluno: horario.id,
+                sala: horario.sala
+            }
         });
-        
+    });
+}
+
+function insiraHorarios(Horarios, req, res) {
+
+    req.body.horarios.forEach(horario => {
+        Horarios.create(horario);
+    });
+}
+
+function obtenhaHorariosAluno(Horarios, req, res) {
+    Horarios.findAll({ where: req.params })
+        .then(result => res.json({ horarios: result }))
+        .catch(error => {
+            envieMensagemErro(res, error);
+        });
+
+}
+
+function envieMensagemErro(res, error) {
+    res.status(412).json({
+        msg: `${error.message}, ${error.instance}, ${error.path}, ${error.type}`
+    });
 }
